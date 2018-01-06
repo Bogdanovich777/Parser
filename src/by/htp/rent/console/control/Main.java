207e45eb12124dc2adff3562ae821d5faeae7f3d
Bogdanovich777 <32797329+Bogdanovich777@users.noreply.github.com>
2018-01-06 12:57:40 +0300

    Add files via upload

---
+++ src/by/htp/rent/console/control/Main.java
@@ -0,0 +1,10 @@
+package by.htp.rent.console.control;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/by/htp/rent/dao/parser/dom/CatalogDataDomImpl.java
@@ -0,0 +1,5 @@
+package by.htp.rent.dao.parser.dom;
+
+public class CatalogDataDomImpl {
+
+}
